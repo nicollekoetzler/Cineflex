b3629726eecd0954a64dc78489c4e55d0444c920
nicollekoetzler <nickkoetzler@gmail.com>
2022-05-19 15:11:06 -0300

    conclui layout

---
+++ src/components/App.js
@@ -0,0 +1,340 @@
+import React from "react";
+import "../assets/css/reset.css";
+import "../assets/css/style.css";
+
+export default function App(){
+
+    return(
+        <>
+            <div className="select-film hidden">
+                <div className="up">
+                    <div className="header">
+                        <h1>CineFlex</h1>
+                    </div>
+                    <div className="subheader">
+                        <p>Selecione o filme:</p>
+                    </div>
+                </div>
+                <div className="down">
+                    <div className="par">
+                        <div className="emcartaz">
+                            <img src="https://www.magazine-hd.com/apps/wp/wp-content/uploads/2022/04/doutor-estranho-2-poster.jpg" />
+                        </div>
+                        <div className="emcartaz">
+                            <img src="https://br.web.img3.acsta.net/pictures/19/08/24/20/41/5572826.jpg"/>
+                        </div>
+                    </div>
+                    <div className="par">
+                        <div className="emcartaz">
+                            <img src="https://rotacult.com.br/wp-content/uploads/2022/03/Top-Gun-Maverick.png"/>
+                        </div>
+                        <div className="emcartaz">
+                            <img src="https://i.pinimg.com/originals/f0/2a/4d/f02a4d54290577864ee41ada8bea5706.jpg"/>
+                        </div>
+                    </div>
+                    <div className="par">
+                        <div className="emcartaz">
+                            <img src="https://ovicio.com.br/wp-content/uploads/2019/09/20190915-spider-man-into-the-spider-verse-poster-martin-ansin-mondo-683x1024.jpg"/>
+                        </div>
+                        <div className="emcartaz">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmdE4GAsQoLUL5l2x3dZjO4aayYqBzbKh-cw&usqp=CAU"/>
+                        </div>
+                    </div>
+                    <div className="par">
+                        <div className="emcartaz">
+                            <img src=""/>
+                        </div>
+                        <div className="emcartaz">
+                            <img src=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="page-select-hour hidden">
+                <div className="up">
+                    <div className="header">
+                        <h1>CineFlex</h1>
+                    </div>
+                    <div className="subheader">
+                        <p>Selecione o horário:</p>
+                    </div>
+                </div>
+                <div className="down2">
+                    <div className="container-data">
+                        <div className="data">
+                            <p>Quinta-feira - 24/06/2021</p>
+                        </div>
+                        <div className="horas">
+                            <div className="sub-hora">
+                                <p>15:00</p>
+                            </div>
+                            <div className="sub-hora">
+                                <p>19:00</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container-data">
+                        <div className="data">
+                            <p>Quinta-feira - 24/06/2021</p>
+                        </div>
+                        <div className="horas">
+                            <div className="sub-hora">
+                                <p>15:00</p>
+                            </div>
+                            <div className="sub-hora">
+                                <p>19:00</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <div className="miniatura">
+                        <div className="miniatura-poster"></div>
+                    </div>
+                    <p>Doutor Estranho no Multiverso da Loucura</p>
+                </div>
+            </div>
+
+            <div className="page-select-chair hidden">
+                <div className="up">
+                    <div className="header">
+                        <h1>CineFlex</h1>
+                    </div>
+                    <div className="subheader">
+                        <p>Selecione o(s) assento(s):</p>
+                    </div>
+                </div>
+                <div className="down3">
+                    <div className="assentos">
+                        <div className="container-balls">
+                            <div className="ball">
+                                <p>01</p>
+                            </div>
+                            <div className="ball">
+                                <p>02</p>
+                            </div>
+                            <div className="ball">
+                                <p>03</p>
+                            </div>
+                            <div className="ball">
+                                <p>04</p>
+                            </div>
+                            <div className="ball">
+                                <p>05</p>
+                            </div>
+                            <div className="ball">
+                                <p>06</p>
+                            </div>
+                            <div className="ball">
+                                <p>07</p>
+                            </div>
+                            <div className="ball">
+                                <p>08</p>
+                            </div>
+                            <div className="ball">
+                                <p>09</p>
+                            </div>
+                            <div className="ball">
+                                <p>10</p>
+                            </div>
+                        </div>
+                        <div className="container-balls">
+                            <div className="ball">
+                                <p>01</p>
+                            </div>
+                            <div className="ball">
+                                <p>02</p>
+                            </div>
+                            <div className="ball">
+                                <p>03</p>
+                            </div>
+                            <div className="ball">
+                                <p>04</p>
+                            </div>
+                            <div className="ball">
+                                <p>05</p>
+                            </div>
+                            <div className="ball">
+                                <p>06</p>
+                            </div>
+                            <div className="ball">
+                                <p>07</p>
+                            </div>
+                            <div className="ball">
+                                <p>08</p>
+                            </div>
+                            <div className="ball">
+                                <p>09</p>
+                            </div>
+                            <div className="ball">
+                                <p>10</p>
+                            </div>
+                        </div>
+                        <div className="container-balls">
+                            <div className="ball">
+                                <p>01</p>
+                            </div>
+                            <div className="ball">
+                                <p>02</p>
+                            </div>
+                            <div className="ball">
+                                <p>03</p>
+                            </div>
+                            <div className="ball">
+                                <p>04</p>
+                            </div>
+                            <div className="ball">
+                                <p>05</p>
+                            </div>
+                            <div className="ball">
+                                <p>06</p>
+                            </div>
+                            <div className="ball">
+                                <p>07</p>
+                            </div>
+                            <div className="ball">
+                                <p>08</p>
+                            </div>
+                            <div className="ball">
+                                <p>09</p>
+                            </div>
+                            <div className="ball">
+                                <p>10</p>
+                            </div>
+                        </div>
+                        <div className="container-balls">
+                            <div className="ball">
+                                <p>01</p>
+                            </div>
+                            <div className="ball">
+                                <p>02</p>
+                            </div>
+                            <div className="ball">
+                                <p>03</p>
+                            </div>
+                            <div className="ball">
+                                <p>04</p>
+                            </div>
+                            <div className="ball">
+                                <p>05</p>
+                            </div>
+                            <div className="ball">
+                                <p>06</p>
+                            </div>
+                            <div className="ball">
+                                <p>07</p>
+                            </div>
+                            <div className="ball">
+                                <p>08</p>
+                            </div>
+                            <div className="ball">
+                                <p>09</p>
+                            </div>
+                            <div className="ball">
+                                <p>10</p>
+                            </div>
+                        </div>
+                        <div className="container-balls">
+                            <div className="ball">
+                                <p>01</p>
+                            </div>
+                            <div className="ball">
+                                <p>02</p>
+                            </div>
+                            <div className="ball">
+                                <p>03</p>
+                            </div>
+                            <div className="ball">
+                                <p>04</p>
+                            </div>
+                            <div className="ball">
+                                <p>05</p>
+                            </div>
+                            <div className="ball">
+                                <p>06</p>
+                            </div>
+                            <div className="ball">
+                                <p>07</p>
+                            </div>
+                            <div className="ball">
+                                <p>08</p>
+                            </div>
+                            <div className="ball">
+                                <p>09</p>
+                            </div>
+                            <div className="ball">
+                                <p>10</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="status">
+                    <div className="container-selecionado">
+                        <div className="selecionado"></div>
+                        <p>Selecionado</p>
+                    </div>
+                    <div className="container-disponivel">
+                        <div className="disponivel"></div>
+                        <p>Disponível</p>
+                    </div>
+                    <div className="container-indisponivel">
+                        <div className="indisponivel"></div>
+                        <p>Indisponivel</p>
+                    </div>
+                </div>
+                <div className="dados">
+                    <div className="nome">
+                        <p>Nome do comprador:</p>
+                        <input placeholder="Digite seu nome..."></input>
+                    </div>
+                    <div className="cpf">
+                        <p>CPF do comprador:</p>
+                        <input placeholder="Digite seu CPF..."></input>
+                    </div>
+                </div>
+                <div className="container-button">
+                    <button>Reservar assento(s)</button>
+                </div>
+                <div className="bottom">
+                    <div className="miniatura">
+                        <div className="miniatura-poster"></div>
+                    </div>
+                    <p>Doutor Estranho no Multiverso da Loucura</p>
+                </div>
+            </div>
+
+            <div className="page-success">
+                <div className="up">
+                    <div className="header">
+                        <h1>CineFlex</h1>
+                    </div>
+                    <div className="subheader4">
+                        <p>Pedido feito com sucesso!</p>
+                    </div>
+                </div>
+                <div className="down4">
+                    <div className="container">
+                        <h2>Filme e sessão</h2>
+                        <h3>Enola Holmes</h3>
+                        <p>24/06/2021 15:00</p>
+                    </div>
+                    <div className="container">
+                        <h2>Ingressos</h2>
+                        <h3>Assento 15</h3>
+                        <p>Assento 16</p>
+                    </div>
+                    <div className="container">
+                        <h2>Comprador</h2>
+                        <h3>Nome: João da Silva Sauro</h3>
+                        <p>CPF: 123.456.789-10</p>
+                    </div>
+                    <div className="container-button4">
+                        <button>Voltar pra Home</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
