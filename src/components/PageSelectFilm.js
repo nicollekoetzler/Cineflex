c7f74f002fc0787899cc9be1d632409127927eea
nicollekoetzler <nickkoetzler@gmail.com>
2022-05-20 11:17:11 -0300

    adiciona imagem filmes pela API

--- src/components/PageSelectFilm.js
+++ src/components/PageSelectFilm.js
@@ -1,6 +1,30 @@
+import axios from 'axios';
+import React from 'react';
+
+function EmCartaz({source}) {
+    return(
+        <div className="em-cartaz">
+            <img src={source} />
+        </div>
+    )
+}
+
 export default function PageSelectFilm() {
+
+
+    const [films, setFilms] = React.useState([]);
+
+    
+    React.useEffect(() => {
+        const promise = axios.get('https://mock-api.driven.com.br/api/v5/cineflex/movies')
+    
+        promise.then(response => {
+            setFilms([...response.data])
+        })
+    }, [] );
+
     return (
-        <div className="select-film hidden">
+        <div className="select-film ">
                 <div className="up">
                     <div className="header">
                         <h1>CineFlex</h1>
@@ -10,37 +34,8 @@ export default function PageSelectFilm() {
                     </div>
                 </div>
                 <div className="down">
-                    <div className="par">
-                        <div className="emcartaz">
-                            <img src="https://www.magazine-hd.com/apps/wp/wp-content/uploads/2022/04/doutor-estranho-2-poster.jpg" />
-                        </div>
-                        <div className="emcartaz">
-                            <img src="https://br.web.img3.acsta.net/pictures/19/08/24/20/41/5572826.jpg"/>
-                        </div>
-                    </div>
-                    <div className="par">
-                        <div className="emcartaz">
-                            <img src="https://rotacult.com.br/wp-content/uploads/2022/03/Top-Gun-Maverick.png"/>
-                        </div>
-                        <div className="emcartaz">
-                            <img src="https://i.pinimg.com/originals/f0/2a/4d/f02a4d54290577864ee41ada8bea5706.jpg"/>
-                        </div>
-                    </div>
-                    <div className="par">
-                        <div className="emcartaz">
-                            <img src="https://ovicio.com.br/wp-content/uploads/2019/09/20190915-spider-man-into-the-spider-verse-poster-martin-ansin-mondo-683x1024.jpg"/>
-                        </div>
-                        <div className="emcartaz">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmdE4GAsQoLUL5l2x3dZjO4aayYqBzbKh-cw&usqp=CAU"/>
-                        </div>
-                    </div>
-                    <div className="par">
-                        <div className="emcartaz">
-                            <img src=""/>
-                        </div>
-                        <div className="emcartaz">
-                            <img src=""/>
-                        </div>
+                    <div className="container-filmes">
+                        {films.map(value => <EmCartaz source={value.posterURL} />)}
                     </div>
                 </div>
             </div>
