194a4fd09d6acb505d180b2647542da510a12837
nicollekoetzler <nickkoetzler@gmail.com>
2022-05-20 22:43:57 -0300

    adiciona rota pagina principal para sessoes respectivas de cada filme

--- src/components/PageSelectFilm.js
+++ src/components/PageSelectFilm.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 import React from 'react';
 
-function EmCartaz({source}) {
+function EmCartaz({source, id}) {
 
     return(
-        <div className="em-cartaz">
-            <img src={source} />
-        </div>
+        <Link to={`/sessoes/${id} `}>
+            <div className="em-cartaz">
+                <img src={source} />
+            </div>
+        </Link>
     )
 }
 
@@ -25,16 +28,16 @@ export default function PageSelectFilm() {
     return (
         <div className="select-film ">
                 <div className="up">
-                    <div className="header">
-                        <h1>CineFlex</h1>
-                    </div>
+                        <div className="header">
+                            <h1>CineFlex</h1>
+                        </div>
                     <div className="subheader">
                         <p>Selecione o filme:</p>
                     </div>
                 </div>
                 <div className="down">
                     <div className="container-filmes">
-                        {films.map(value => <EmCartaz source={value.posterURL} />)}
+                        {films.map(value => <EmCartaz source={value.posterURL} id={value.id} />)}
                     </div>
                 </div>
             </div>
--- src/components/PageSelectHour.js
+++ src/components/PageSelectHour.js
@@ -1,6 +1,7 @@
 import {useParams} from 'react-router-dom';
 import React from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 export default function PageSelectHour(){
@@ -14,14 +15,16 @@ export default function PageSelectHour(){
         promise.then(response => {
             setFilms({...response.data})
         })
-    })
+    }, [])
 
     return (
         <div className="page-select-hour">
                 <div className="up">
+                <Link style={{textDecoration: "none", color: "black"}} to={"/"}>
                     <div className="header">
                         <h1>CineFlex</h1>
                     </div>
+                </Link>
                     <div className="subheader">
                         <p>Selecione o horário:</p>
                     </div>
